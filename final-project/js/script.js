616e7d3d34972687530db33a8a75525957da1508
frostyzp <91027903+frostyzp@users.noreply.github.com>
2023-04-30 15:54:09 -0400

    Updated click + Drag events

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,21 +1,61 @@
+// Store show items in a set
+const showSet = new Set();
 
-// Scatter different elements across the screen/page
+class Show {
+  constructor(title, year, description, img) {
+          this.title = title;
+          this.year = year;
+          this.description = description;
+          this.img = img;
+
+          this.imgSrc = 'shows/' +this.img + '.jpg'
+          this.element = null;
+      }
+  }
+
+// GET REFERENCES TO THE CONTAINER ELEMENT IN HTML FILE
+const container = document.getElementById('container');
 const items = document.querySelectorAll('.item');
-console.log(items)
 
-items.forEach((box, index) => {
-    const randomInt = Math.random(); // Generates a random integer between 1 and 10 (inclusive)
-    console.log(randomInt)
-    multiplier = 1;
+// CREATE OBJECTS FROM THE ARRAY (SHOWS)
+  for (const showtest in shows) {
+    console.log("Created Objects", shows[showtest]);
+    showObj = shows[showtest]
+    addShows(showObj.title, showObj.year, showObj.description, showObj.img);
+  }
 
-    // if (randomInt >= 0.5) {
-    //     multiplier -= 60;
-    //   }
+// CREATE DOM ELEMENTS FOR EACH ITEM
+for (const showItem of showSet) {
+  console.log('SHOWITEM', showItem);
+  createElement(showItem);
+}
 
-  const leftPosition = (index / items.length) * 30;
-  const vertPosition = (index / items.length) * 100 * multiplier;
+// CREATE ELEMENTS (HTML) TO BE DISPLAYED
+function createElement(showItem){
+  const template = document.querySelector('#show-template');
+  const clone = template.content.cloneNode(true);
+  showItem.element = clone.querySelector('.item');
 
+  const showListElement = document.querySelector('#show-list');
+  showListElement.append(showItem.element);
 
-  box.style.left = `${leftPosition}%`;
-  box.style.top = `${vertPosition}%`
-});
+  updateElement(showItem);
+}
+
+// UPDATE ELEMENTS BASED ON JS ARRAY - IMG, YEARS, DESCRIPTION ETC.
+function updateElement(showItem){
+    const showImgElement = showItem.element.querySelector('.show');
+    showImgElement.src = 'assets/shows/' +showItem.img + '.jpg';
+
+    console.log('UPDATE / UPDATE', showItem.element)
+    showItem.element.dataset.year = showItem.title;
+    showItem.element.dataset.description = showItem.description;
+    // const newElement = document.createElement('p');
+}
+
+// ADD SHOW OBJECTS TO A SET WHICH WILL BE RENDERED ONTO THE PAGE
+function addShows(title, year, description, img){
+  const showItem = new Show (title, year, description, img);
+  showSet.add(showItem);
+  return showItem
+}
---
+++ final-project/js/shows.js
@@ -0,0 +1,74 @@
+const shows = {
+    "bleach": {
+        "title": "bleach",
+        "year": "2004-2007",
+        "description": "bleach ppoopy",
+        "img": "bleach"
+    },
+    "teentitans": {
+        "title": "Teen Titans",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "teentitans"
+
+    },
+    "toradora": {
+        "title": "bleach",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "bleach"
+
+    },
+    "bleach4": {
+        "title": "bleach",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "bleach"
+
+    },
+    "bleach5": {
+        "title": "bleach",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "bleach"
+
+    },
+
+    "bleach6": {
+        "title": "bleach",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "bleach"
+
+    },
+
+
+    "bleach7": {
+        "title": "bleach",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "bleach"
+
+    },
+
+
+    "bleach8": {
+        "title": "bleach",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "bleach"
+
+    },
+
+
+    "bleach9": {
+        "title": "bleach",
+        "year": "original-cinnamon-roll.jpg",
+        "description": "DESCRIPTION",
+        "img": "bleach"
+
+    },
+
+
+
+};
