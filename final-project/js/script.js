58e0d097576e4fad51f98c8f564aa3aa380545a8
frostyzp <91027903+frostyzp@users.noreply.github.com>
2023-05-01 21:19:28 -0400

    Added about page, fixed landing page

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -2,11 +2,12 @@
 const showSet = new Set();
 
 class Show {
-  constructor(title, year, description, img) {
+  constructor(title, year, creator, description, img) {
           this.title = title;
           this.year = year;
           this.description = description;
           this.img = img;
+          this.creator = creator;
 
           this.imgSrc = 'shows/' +this.img + '.jpg'
           this.element = null;
@@ -15,14 +16,20 @@ class Show {
 
 // GET REFERENCES TO THE CONTAINER ELEMENT IN HTML FILE
 const container = document.getElementById('container');
-const items = document.querySelectorAll('.item');
 
 // CREATE OBJECTS FROM THE ARRAY (SHOWS)
   for (const showtest in shows) {
     console.log("Created Objects", shows[showtest]);
     showObj = shows[showtest]
-    addShows(showObj.title, showObj.year, showObj.description, showObj.img);
+    addShows(showObj.title, showObj.year, showObj.creator, showObj.description, showObj.img);
   }
+
+// ADD SHOW OBJECTS TO A SET WHICH WILL BE RENDERED ONTO THE PAGE
+function addShows(title, year, creator, description, img){
+  const showItem = new Show (title, year, creator, description, img);
+  showSet.add(showItem);
+  return showItem
+}
 
 // CREATE DOM ELEMENTS FOR EACH ITEM
 for (const showItem of showSet) {
@@ -48,16 +55,14 @@ function updateElement(showItem){
     showImgElement.src = 'assets/shows/' +showItem.img + '.jpg';
     showImgElement.alt = 'COVER IMAGE OF ' +showItem.title 
 
-    console.log('UPDATE / UPDATE', showItem.element)
     showItem.element.dataset.title = showItem.title;
     showItem.element.dataset.year = showItem.year;
+    showItem.element.dataset.creator = showItem.creator;
     showItem.element.dataset.description = showItem.description;
-    // const newElement = document.createElement('p');
+    showItem.element.dataset.category = showItem.year;
 }
 
-// ADD SHOW OBJECTS TO A SET WHICH WILL BE RENDERED ONTO THE PAGE
-function addShows(title, year, description, img){
-  const showItem = new Show (title, year, description, img);
-  showSet.add(showItem);
-  return showItem
-}
+
+
+
+
--- final-project/js/shows.js
+++ final-project/js/shows.js
@@ -2,19 +2,22 @@ const shows = {
     "inuyasha": {
         "title": "INUYASHA",
         "year": "2000-2004 (167 EPISODES)",
+        "creator": "Rumiko Takahashi",
         "description": "InuYasha is a popular Japanese anime and manga series that was created by Rumiko Takahashi. The story follows a high school girl named Kagome Higurashi, who is transported back in time to feudal Japan, where she meets the half-demon Inuyasha. Together, they embark on a quest to recover the shards of the powerful Jewel of Four Souls, which has been shattered and scattered across the land.In addition to the anime, InuYasha also spawned four feature films, a sequel anime series titled InuYasha: The Final Act, and several video games",
         "img": "inuyasha"
     },
     "teentitans": {
         "title": "FRUITS BASKET",
         "year": "2001 (63 episodes)",
+        "creator": "Rumiko Takahashi",
         "description": "Fruits Basket is a popular Japanese anime and manga series that was created by Natsuki Takaya. The story follows an orphaned high school girl named Tohru Honda, who is taken in by the wealthy Soma family. Tohru soon discovers that members of the Soma family are cursed by the spirits of the Chinese zodiac, causing them to transform into their respective animal forms when hugged by someone of the opposite sex.",
         "img": "teentitans"
 
     },
     "toradora": {
-        "title": "bleach",
-        "year": "original-cinnamon-roll.jpg",
+        "title": "toradora",
+        "year": "2002",
+        "creator": "Rumiko Takahashi",
         "description": "DESCRIPTION",
         "img": "bleach"
 
@@ -22,6 +25,7 @@ const shows = {
     "bleach4": {
         "title": "bleach",
         "year": "original-cinnamon-roll.jpg",
+        "creator": "Rumiko Takahashi",
         "description": "DESCRIPTION",
         "img": "bleach"
 
@@ -29,46 +33,10 @@ const shows = {
     "bleach5": {
         "title": "bleach",
         "year": "original-cinnamon-roll.jpg",
+        "creator": "Rumiko Takahashi",
         "description": "DESCRIPTION",
         "img": "bleach"
-
-    },
-
-    "bleach6": {
-        "title": "bleach",
-        "year": "original-cinnamon-roll.jpg",
-        "description": "DESCRIPTION",
-        "img": "bleach"
-
-    },
-
-
-    "bleach7": {
-        "title": "bleach",
-        "year": "original-cinnamon-roll.jpg",
-        "description": "DESCRIPTION",
-        "img": "bleach"
-
-    },
-
-
-    "bleach8": {
-        "title": "bleach",
-        "year": "original-cinnamon-roll.jpg",
-        "description": "DESCRIPTION",
-        "img": "bleach"
-
-    },
-
-
-    "bleach9": {
-        "title": "bleach",
-        "year": "original-cinnamon-roll.jpg",
-        "description": "DESCRIPTION",
-        "img": "bleach"
-
-    },
-
+    }
 
 
 };
