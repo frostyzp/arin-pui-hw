dcd028dfab7c89feb9b93b10847ba2f181b8fc36
frostyzp <91027903+frostyzp@users.noreply.github.com>
2023-05-01 22:58:47 -0400

    pre-final submission

--- final-project/js/shows.js
+++ final-project/js/shows.js
@@ -2,41 +2,129 @@ const shows = {
     "inuyasha": {
         "title": "INUYASHA",
         "year": "2000-2004 (167 EPISODES)",
-        "creator": "Rumiko Takahashi",
-        "description": "InuYasha is a popular Japanese anime and manga series that was created by Rumiko Takahashi. The story follows a high school girl named Kagome Higurashi, who is transported back in time to feudal Japan, where she meets the half-demon Inuyasha. Together, they embark on a quest to recover the shards of the powerful Jewel of Four Souls, which has been shattered and scattered across the land.In addition to the anime, InuYasha also spawned four feature films, a sequel anime series titled InuYasha: The Final Act, and several video games",
+        "creator": "RUMIKO TAKAHASHI",
+        "description": "InuYasha is a popular Japanese anime and manga series that was created by Rumiko Takahashi. The story follows a high school girl named Kagome Higurashi, who is transported back in time to feudal Japan, where she meets the half-demon Inuyasha. Together, they embark on a quest to recover the shards of the powerful Jewel of Four Souls, which has been shattered and scattered across the land. <br><br>In addition to the anime, InuYasha also led to four feature films, a sequel anime series titled InuYasha: The Final Act, and several video games",
         "img": "inuyasha"
     },
-    "teentitans": {
+    "fruitsbasket": {
         "title": "FRUITS BASKET",
-        "year": "2001 (63 episodes)",
-        "creator": "Rumiko Takahashi",
-        "description": "Fruits Basket is a popular Japanese anime and manga series that was created by Natsuki Takaya. The story follows an orphaned high school girl named Tohru Honda, who is taken in by the wealthy Soma family. Tohru soon discovers that members of the Soma family are cursed by the spirits of the Chinese zodiac, causing them to transform into their respective animal forms when hugged by someone of the opposite sex.",
-        "img": "teentitans"
+        "year": "2001 (63 EPISODES)",
+        "creator": "Natsuki Takaya",
+        "description": "The story follows an orphaned high school girl named Tohru Honda, who is taken in by the wealthy Soma family. Tohru soon discovers that members of the Soma family are cursed by the spirits of the Chinese zodiac, causing them to transform into their respective animal forms when hugged by someone of the opposite sex.",
+        "img": "fruitsbasket"
+    },
+    "fairlyoddParents": {
+        "title": "FAIRLY ODDPARENTS",
+        "year": "2001-2017 (10 SEASONS)",
+        "creator": "BUTCH HARTMAN",
+        "description": " The show follows the adventures of a 10-year-old boy named Timmy Turner, who is granted two fairy godparents, Cosmo and Wanda, who can grant him wishes. <br><br>Fairly OddParents has won multiple awards, including two Emmy Awards, and has been praised for its humor and heartwarming themes. The show's popularity led to several spin-off series and movies, including the live-action adaptation A Fairly Odd Movie: Grow Up, Timmy Turner",
+        "img": "fairlyoddparents"
+    },
+    "jimmyneutron": {
+        "title": "JIMMY NEUTRON",
+        "year": "2002-2006 (3 SEASONS)",
+        "creator": "JOHN A. DAVIS",
+        "description": "The show follows the adventures of a young boy genius named Jimmy Neutron, who invents a variety of gadgets and machines to help him navigate the challenges of everyday life. <br><br>The show was nominated for several awards, including nine Emmy Awards, and won a Kids' Choice Award for Favorite Cartoon in 2003. The popularity of the show led to several spin-off media, including video games, comic books, and a crossover special with The Fairly OddParents.",
+        "img": "jimmyneutron"
 
+    },
+    "codenamekidsnextdoor": {
+        "title": "CODENAME: KIDS NEXT DOOR",
+        "year": "2002-2008 (6 SEASONS)",
+        "creator": "TOM WARBURTON",
+        "description": "The show follows the adventures of a group of five 10-year-old kids, who operate a secret organization known as the Kids Next Door. The group's mission is to fight against adult tyranny and protect the rights of kids everywhere. <br><br>Codename: Kids Next Door also inspired several movies and video games, as well as a comic book series. The show was nominated for several awards, including five Emmy Awards, and won an Annie Award for Best Animated Television Production in 2004.",
+        "img": "codenamekidsnextdoor"
+    },
+    "naruto": {
+        "title": "NARUTO",
+        "year": "2002-2007 (220 EPISODES)",
+        "creator": "MASASHI KISHIMOTO",
+        "description": "The story follows a young orphaned ninja named Naruto Uzumaki, who dreams of becoming the Hokage, the leader of his village. <br><br>The anime adaptation of Naruto was produced by Studio Pierrot and directed by Hayato Date.The anime was followed by a sequel series, Naruto Shippuden, which continued the story and aired from 2007 to 2017. s. The series has also spawned several feature films, video games, and other merchandise. Naruto remains one of the most popular anime and manga series of all time and has influenced countless other works in the genre.",
+        "img": "naruto"
+    },
+    "teentitans": {
+        "title": "TEEN TITANS",
+        "year": "2003-2006 (5 SEASONS)",
+        "creator": "GLEN MURAKAMI",
+        "description": "The show follows a group of young superheroes, including Robin, Starfire, Raven, Beast Boy, and Cyborg, as they fight against various villains and save the world from destruction. <br><br>The popularity of Teen Titans led to several spin-off media, including video games, comic books, and a 2018 revival series titled Teen Titans Go!. The original series has been nominated for several awards, including two Primetime Emmy Awards, and has won a Daytime Emmy Award for Outstanding Special Class Animated Program",
+        "img": "teentitans"
+    },
+    "fullmetalalchemist": {
+        "title": "FULLMETAL ALCHEMIST",
+        "year": "2003-2004 (51 EPISODES)",
+        "creator": "HIROMU ARAKAWA",
+        "description": "he story follows two brothers, Edward and Alphonse Elric, who use alchemy to try and bring their deceased mother back to life, resulting in a tragic accident that leaves Edward without an arm and a leg and Alphonse's soul trapped in a suit of armor. <br><br>The anime was followed by a second adaptation, Fullmetal Alchemist: Brotherhood, which more closely follows the manga and aired from 2009 to 2010. The series has also spawned several feature films, video games, and other merchandise. Fullmetal Alchemist remains one of the most popular and beloved anime and manga series of all time and has influenced countless other works in the genre.",
+        "img": "fullmetalalchemist"
+    },
+    "dannyphantom": {
+        "title": "DANNY PHANTOM",
+        "year": "2004-2007 (3 SEASONS)",
+        "creator": "BUTCH HARTMAN",
+        "description": "The show follows the adventures of a teenage boy named Danny Fenton, who gains ghostly powers after an accident in his parents' laboratory. <br><br>The show was nominated for several awards, including three Primetime Emmy Awards, and won an Annie Award for Best Animated Television Production in 2005.",
+        "img": "dannyphantom"
+    },
+    "bleach": {
+        "title": "BLEACH",
+        "year": "2004-2001 (366 EPISODES)",
+        "creator": "TITE KUBO",
+        "description": "The story follows a high school student named Ichigo Kurosaki, who gains the powers of a Soul Reaper, a celestial being that protects humans from evil spirits. The anime adaptation of Bleach was produced by Studio Pierrot and directed by Noriyuki Abe. <br><br>A new anime adaptation, Bleach: Thousand-Year Blood War Arc, was announced in 2020 and is set to continue the story from where the original anime left off.",
+        "img": "bleach"
+    },
+    "monster": {
+        "title": "MONSTER",
+        "year": "2004-2005 (74 EPISODES)",
+        "creator": "NAOKI URASAWA",
+        "description": "Monster is a psychological thriller anime series based on the manga of the same name by Naoki Urasawa. The story follows Dr. Kenzo Tenma, a skilled neurosurgeon who saves the life of a young boy, Johan Liebert, instead of the mayor of the city, who was supposed to be operated on first. Tenma later learns that Johan is a psychopath responsible for a series of murders and sets out to stop him. <br><br>The anime adaptation of Monster was produced by Madhouse and directed by Masayuki Kojima. The series has won several awards, including the prestigious Tezuka Osamu Cultural Prize, and has been critically acclaimed for its masterful storytelling and thought-provoking commentary on human nature",
+        "img": "monster"
+    },
+    "death note": {
+        "title": "DEATH NOTE",
+        "year": "2006 (36 EPISODES)",
+        "creator": "TSUGUMI OHBA, TAKESHI OBATA",
+        "description": "The story follows a high school student named Light Yagami who discovers a supernatural notebook that can kill anyone whose name is written in it. Light decides to use the notebook to rid the world of criminals and becomes a vigilante known as 'Kira.' However, his actions attract the attention of a brilliant detective named L, who sets out to capture Kira. <br><br>The anime adaptation of Death Note was produced by Madhouse and directed by Tetsuro Araki. The series has been widely popular, with numerous adaptations, including a live-action film and TV drama, and spin-off media such as light novels, video games, and a musical.",
+        "img": "deathnote"
+    },
+    "phineasandferb": {
+        "title": "PHINEAS AND FERB",
+        "year": "2007-2015 (4 SEASONS)",
+        "creator": "DAN POVENMIRE, JEFF 'SWAMPY' MARSH",
+        "description": "The Disney Channel show follows two stepbrothers, Phineas Flynn and Ferb Fletcher, as they embark on creative and imaginative projects during their summer vacation, often leading to wild and wacky adventures. <br><br>The show's popularity led to several spin-off media, including a video game series, live shows, and a feature-length movie titled Phineas and Ferb the Movie: Candace Against the Universe, which premiered on Disney+ in 2020",
+        "img": "phineasandferb"
+    },
+    "ouranhighschoolhostclub": {
+        "title": "OURAN HIGH SCHOOL HOST CLUB",
+        "year": "2007 (26 EPISODES)",
+        "creator": "BISCO HATORI",
+        "description": "Ouran High School Host Club is a romantic comedy anime series based on the manga of the same name by Bisco Hatori. The story follows Haruhi Fujioka, a bright and studious student who gains admission to the prestigious Ouran High School on a scholarship. She accidentally stumbles into the school's Host Club, a group of handsome boys who entertain female clients with their charm and good looks, and ends up becoming a host to repay a debt. The anime adaptation of Ouran High School Host Club was produced by Bones and directed by Takuya Igarashi. <br><br>The series has been widely popular, with numerous adaptations, including a live-action TV drama, a video game, and a stage musical.",
+        "img": "ouran"
+    },
+    "chowder": {
+        "title": "CHOWDER",
+        "year": "2007-2010 (3 SEASONS)",
+        "creator": "C.H. GREENBLATT",
+        "description": "The Cartoon Network show follows the adventures of a young apprentice chef named Chowder, who dreams of becoming a great chef like his mentor, Mung Daal. Along with his friends, Mung Daal's wife Truffles and fellow apprentice Panini, Chowder navigates the strange and whimsical world of the culinary arts. <br><br>The show has inspired merchandise, such as toys, games, and clothing, and has even been referenced in other media, such as video games and TV shows.",
+        "img": "chowder"
     },
     "toradora": {
-        "title": "toradora",
-        "year": "2002",
-        "creator": "Rumiko Takahashi",
-        "description": "DESCRIPTION",
-        "img": "bleach"
-
+        "title": "TORADORA",
+        "year": "2008-2009 (25 EPISODES)",
+        "creator": "YUYUKO TAKEMIYA",
+        "description": "Toradora! is a romantic comedy anime series based on the light novel series of the same name by Yuyuko Takemiya. The story follows Ryuuji Takasu, a high school student who is often misunderstood due to his intimidating appearance, and Taiga Aisaka, a tiny but fiery girl who also attends the same school. The two form an unlikely friendship and agree to help each other win over their respective crushes. <br><br>The anime adaptation of Toradora! was produced by J.C.Staff and directed by Tatsuyuki Nagai. <br> The series has been widely popular and has inspired various adaptations, including a manga series, light novel spin-offs, and a live-action film adaptation.",
+        "img": "toradora"
     },
-    "bleach4": {
-        "title": "bleach",
-        "year": "original-cinnamon-roll.jpg",
-        "creator": "Rumiko Takahashi",
-        "description": "DESCRIPTION",
-        "img": "bleach"
-
+    "adventuretime": {
+        "title": "ADVENTURE TIME",
+        "year": "2010-2018 (10 SEASONS)",
+        "creator": "PENDLETON WARD",
+        "description": " The Cartoon Network show follows the adventures of a young boy named Finn and his best friend and adoptive brother, a dog named Jake, in the post-apocalyptic Land of Ooo. Together, Finn and Jake go on various quests and battles against evil forces to protect the land and its inhabitants.<br> The series has been widely popular and has won numerous awards, including six Primetime Emmy Awards. It has also inspired various adaptations, including comic books, video games, and a feature-length animated film",
+        "img": "adventuretime"
     },
-    "bleach5": {
-        "title": "bleach",
-        "year": "original-cinnamon-roll.jpg",
-        "creator": "Rumiko Takahashi",
-        "description": "DESCRIPTION",
-        "img": "bleach"
-    }
-
+    "phineasandferb": {
+        "title": "PHINEAS AND FERB",
+        "year": "2007-2015 (4 SEASONS)",
+        "creator": "DAN POVENMIRE, JEFF 'SWAMPY' MARSH",
+        "description": "XXX",
+        "img": "phineasandferb"
+    }   
 
 };
